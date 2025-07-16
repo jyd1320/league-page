5abb6d8fe0caba53be9e6c806e9c6acabbe3eeb3
jyd1320 <dmedi44@wgu.edu>
2025-07-15 22:05:57 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -96,7 +96,7 @@ export const managers = [
   and will allow updates without causing merge conflicts
   */
   
-    {
+    // {
       // "roster": 3,  // (DEPRECATED! Don't use this anymore) ID of the roster that the manager manages (look at the order of the power rankings graph)
       // managerID; "1226957684180258816",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
       // name; "JYD",
@@ -118,5 +118,5 @@ export const managers = [
       // "philosophy": "Your fantasy team's philosophy", // (optional)
       // "tradingScale": 10, // 1 - 10 (optional)
       // "preferredContact": "Text",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-    },
+    // },
     
