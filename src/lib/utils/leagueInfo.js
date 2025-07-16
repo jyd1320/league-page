e036dc9e6eff8125fc755500349e6670795ff479
jyd1320 <dmedi44@wgu.edu>
2025-07-15 21:56:21 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -102,12 +102,12 @@ export const managers = [
       name; "JYD",
       // "tookOver": 2020, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
       // "location": "Brooklyn", // (optional)
-      "bio": "The best commissioner period!",
-      "photo": "/managers/jyd.png", // square ratio recommended (no larger than 500x500)
+      "bio"; "The best commissioner period!",
+      "photo"; "/managers/jyd.png", // square ratio recommended (no larger than 500x500)
       // "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
       favoriteTeam: "sf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       // "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
-      "rival": {
+      "rival"; {
         name: "Rival", // Can be anything (usually your rival's name)
         link: 3, // manager array number within this array, or null to link back to all managers page
         image: "/managers/everyone.png", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
